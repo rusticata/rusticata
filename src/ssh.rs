0cb513813f610acd5096a659bc4fffb1eddd1462
Pierre Chifflier <chifflier@wzdftpd.net>
2017-08-19 10:33:50 +0200

    SSH: split defragmentation buffers to client/server

--- src/ssh.rs
+++ src/ssh.rs
@@ -17,6 +17,8 @@ pub struct SSHParser<'a> {
     _name: Option<&'a[u8]>,
 
     state: SSHConnectionState,
+    buffer_clt: Vec<u8>,
+    buffer_srv: Vec<u8>,
 }
 
 impl<'a> SSHParser<'a> {
@@ -24,6 +26,8 @@ impl<'a> SSHParser<'a> {
         SSHParser{
             _name: Some(name),
             state: SSHConnectionState::Start,
+            buffer_clt: Vec::new(),
+            buffer_srv: Vec::new(),
         }
     }
 
@@ -39,7 +43,7 @@ impl<'a> SSHParser<'a> {
                         info!("{}", line.to_hex(16));
                     }
                 }
-                debug!("parse_ssh: {:?}",res);
+                debug!("parse_ssh_identification: {:?}",res);
                 self.state = match self.state {
                     SSHConnectionState::Start       => SSHConnectionState::ClientIdent,
                     SSHConnectionState::ClientIdent => SSHConnectionState::ServerIdent,
@@ -48,30 +52,58 @@ impl<'a> SSHParser<'a> {
                 info!("protocol\n{}", res.proto.to_hex(16));
                 info!("software\n{}", res.software.to_hex(16));
             },
-            e @ _ => warn!("parse_ssh: {:?}",e),
+            e @ _ => {
+                warn!("parse_ssh_identification: {:?}",e);
+                self.state = SSHConnectionState::Error;
+            },
         };
         R_STATUS_OK
     }
 
-    fn parse_packet(&mut self, i: &[u8]) -> u32 {
-        match ssh::parse_ssh_packet(i) {
-            IResult::Done(rem,ref res) => {
-                debug!("parse_ssh: {:?}",res);
+    fn parse_packet(&mut self, i: &[u8], direction:u8) -> u32 {
+        debug!("parse_ssh_packet direction: {}", direction);
+        debug!("\tbuffer_clt size: {}", self.buffer_clt.len());
+        debug!("\tbuffer_srv size: {}", self.buffer_srv.len());
+        let mut v : Vec<u8>;
+        // Check if a record is being defragmented
+        let self_buffer =
+            if direction == 0 { &mut self.buffer_srv }
+            else { &mut self.buffer_clt };
+        let buf = match self_buffer.len() {
+            0 => i,
+            _ => {
+                v = self_buffer.split_off(0);
+                v.extend_from_slice(i);
+                v.as_slice()
             },
-            e @ _ => warn!("parse_ssh: {:?}",e),
         };
+        // info!("parsing:\n{}", buf.to_hex(16));
+        match ssh::parse_ssh_packet(buf) {
+            IResult::Done(rem,ref res) => {
+                debug!("parse_ssh_packet: {:?}",res);
+            },
+            IResult::Incomplete(_e) => {
+                debug!("Defragmentation required (SSH packet): {:?}", _e);
+                self_buffer.extend_from_slice(buf);
+            },
+            e @ _ => {
+                warn!("parse_ssh_packet: {:?}",e);
+                self.state = SSHConnectionState::Error;
+            },
+        };
+        // info!("after parsing:\n{}", self_buffer.to_hex(16));
         R_STATUS_OK
     }
 }
 
 
 impl<'a> RParser for SSHParser<'a> {
-    fn parse(&mut self, i: &[u8], _direction: u8) -> u32 {
+    fn parse(&mut self, i: &[u8], direction: u8) -> u32 {
         match self.state {
             SSHConnectionState::Start |
             SSHConnectionState::ClientIdent  => self.parse_ident(i),
             SSHConnectionState::ServerIdent |
-            SSHConnectionState::Next         => self.parse_packet(i),
+            SSHConnectionState::Next         => self.parse_packet(i,direction),
             SSHConnectionState::Error        => R_STATUS_FAIL,
         }
     }
