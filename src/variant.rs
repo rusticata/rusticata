1f6a719b6dbe02c67ed6bdd341e2eb9d112cd3d3
Pierre Chifflier <chifflier@wzdftpd.net>
2019-12-23 15:04:58 +0100

    Add macro to help generating get() and keys() for RParser

--- src/variant.rs
+++ src/variant.rs
@@ -67,6 +67,143 @@ impl<'a> From<&'a String> for Variant<'a> {
     }
 }
 
+#[macro_export]
+macro_rules! gen_get_variants_old {
+    ( impl $( $pattern:expr => |$s:ident| $closure:expr ),* ) => {
+    };
+    ( $t:ident, $( $pattern:expr => |$s:ident| $closure:expr ),* ) => {
+        fn get<'b>(&'b self, key: &str) -> Option<Variant<'b>> {
+            match key {
+                $(
+                    $pattern => {
+                        let closure = |$s: &'b $t| $closure;
+                        closure(self)
+                    },
+                )*
+                _ => None,
+            }
+        }
+    };
+}
+
+
+#[macro_export]
+macro_rules! gen_get_variants {
+    // Entry point
+    ($t:ident, $($body:tt)* ) => {
+        fn get<'b>(&'b self, key: &str) -> Option<Variant<'b>> {
+            gen_get_variants!{ @gen_match $t {}, self, key, $($body)* }
+        }
+        fn keys(&self) -> ::std::slice::Iter<&str> {
+            gen_get_variants!{ @gen_keys [], $($body)* }
+        }
+    };
+    //
+    // MATCH ARMS
+    //
+    // Simple closure case
+    (@gen_match $t:ident {$($arms:tt)*}, $self:ident, $key:ident, $pattern:pat => |$s:ident| $closure:expr, $($tail:tt)*) => {
+        gen_get_variants!{
+            @gen_match
+            $t
+            {
+                $($arms)*
+                $pattern => {
+                    let closure = |$s: &'b $t| $closure;
+                    closure($self)
+                },
+            },
+            $self,
+            $key,
+            $($tail)*
+        }
+    };
+    // Shortcut: into
+    (@gen_match $t:ident {$($arms:tt)*}, $self:ident, $key:ident, $pattern:ident => into, $($tail:tt)*) => {
+        gen_get_variants!{
+            @gen_match
+            $t
+            {
+                $($arms)*
+                stringify!{$pattern} => {
+                    Some($self.$pattern.into())
+                },
+            },
+            $self,
+            $key,
+            $($tail)*
+        }
+    };
+    // Shortcut: map
+    (@gen_match $t:ident {$($arms:tt)*}, $self:ident, $key:ident, $pattern:ident => map, $($tail:tt)*) => {
+        gen_get_variants!{
+            @gen_match
+            $t
+            {
+                $($arms)*
+                stringify!{$pattern} => {
+                    $self.$pattern.map(|p| p.into())
+                },
+            },
+            $self,
+            $key,
+            $($tail)*
+        }
+    };
+    // Shortcut: map_as_ref
+    (@gen_match $t:ident {$($arms:tt)*}, $self:ident, $key:ident, $pattern:ident => map_as_ref, $($tail:tt)*) => {
+        gen_get_variants!{
+            @gen_match
+            $t
+            {
+                $($arms)*
+                stringify!{$pattern} => {
+                    $self.$pattern.as_ref().map(|p| p.into())
+                },
+            },
+            $self,
+            $key,
+            $($tail)*
+        }
+    };
+    // Termination rule
+    (@gen_match $t:ident {$($arms:tt)*}, $self:ident, $key:ident, /* $($body:tt)* */  $(,)* ) => {
+        match $key {
+            $($arms)*
+            _ => None
+        }
+    };
+    //
+    // KEYS
+    //
+    // Simple closure case
+    (@gen_keys [$($arms:tt)*], $pattern:tt => |$s:ident| $closure:expr, $($tail:tt)*) => {
+        gen_get_variants!{
+            @gen_keys
+            [
+                $($arms)* $pattern,
+            ],
+            $($tail)*
+        }
+    };
+    // Shortcut: into
+    (@gen_keys [$($arms:tt)*], $pattern:ident => $kw:tt, $($tail:tt)*) => {
+        gen_get_variants!{
+            @gen_keys
+            [
+                $($arms)* stringify!{$pattern},
+            ],
+            $($tail)*
+        }
+    };
+    // Termination rule
+    (@gen_keys [$($keys:tt)*], /* $($body:tt)* */  $(,)* ) => {
+        [
+            $($keys)*
+        ].iter()
+    };
+}
+
 #[cfg(test)]
 mod tests {
     use super::Variant;
