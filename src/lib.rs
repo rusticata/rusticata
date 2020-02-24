b069dbe98b22af4ee4451a46cde19e1a9100caa3
Pierre Chifflier <chifflier@wzdftpd.net>
2020-02-24 09:30:58 +0100

    Start removing C interface

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,6 @@ extern crate nom;
 #[macro_use]
 extern crate log;
 
-use log::LevelFilter;
-
 extern crate itertools;
 
 extern crate md5;
@@ -22,15 +20,6 @@ extern crate snmp_parser;
 extern crate ssh_parser;
 extern crate tls_parser;
 extern crate x509_parser;
-
-const SURICATA_RUST_MAGIC : u32 = 0x1234;
-
-pub use common::*;
-#[macro_use]
-pub mod common;
-
-pub use logger::*;
-pub mod logger;
 
 pub use rparser::*;
 #[macro_use]
@@ -78,39 +67,3 @@ pub use ssh::{SSHBuilder,SSHParser,ssh_probe};
 
 pub use tls::*;
 pub mod tls;
-
-use tls_parser::tls_ciphers::CIPHERS;
-
-/// Rusticata crate init function
-///
-/// This function **must** be called by the client application (Suricata) to initialize the
-/// rusticata library functions.
-///
-/// The argument is a pointer to a configuration structure, containing a magic number,
-/// a pointer to the C log function, and the log level of the client application.
-/// Rusticata will use the same log level, and configure a Rust Logger object to send logs
-/// to Suricata.
-///
-/// The lifetime of the configuration **must** be greater or equal to the one of the
-/// rusticata crate.
-#[no_mangle]
-pub extern "C" fn rusticata_init(config: &'static mut SuricataConfig) -> i32 {
-    assert!(std::ptr::null_mut() != config);
-    unsafe { suricata_config = Some(config) };
-
-    assert_eq!(config.magic,SURICATA_RUST_MAGIC);
-
-    let log_level = match config.log_level {
-        0..=4  => LevelFilter::Error,
-        5      => LevelFilter::Warn,
-        6..=7  => LevelFilter::Info,
-        8..=11 => LevelFilter::Debug,
-        _      => LevelFilter::Off,
-    };
-
-    logger::init(log_level).unwrap();
-
-    info!("Rusticata TLS parser ready, {} ciphers loaded",CIPHERS.len());
-
-    0
-}
